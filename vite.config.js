93f0b9cceeb5ee87657d6f20b4669a82958c7264
JuanSaya <pretelcarlos01@gmail.com>
2025-08-20 14:10:02 -0500

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://JuanSaya.github.io/pa-web'
+  base:'/pa-web2'
 })
